e04da71cd37cdb0fdd40a92fbdad5cd97f384b14
khaled Shiekh Ali <20khaled2001@gmail.com>
2023-08-21 15:52:11 +0300

    new changes 3

--- plugins/chart3.client.ts
+++
@@ -1,7 +0,0 @@
-
-// Workaround because chart.js doesn't provide an default export
-import { Chart, DoughnutController, ArcElement, Tooltip,registerables } from 'chart.js';
-
-export default defineNuxtPlugin(() => {
-    Chart.register(...registerables);
-});
